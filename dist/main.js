54c3d2a600d84a270d9f63cd4f34dc1c6e879eb8
alqmc <alqmc520@gmail.com>
2022-10-20 23:09:20 +0800

    fix: gacm use switch cancel

--- dist/main.js
+++ dist/main.js
@@ -298,10 +298,11 @@ const useUse = async ([name], cmd) => {
         };
       })
     });
-    if (useUser)
-      useUser = user;
-    else
-      return log.error(`user cancels operation`);
+    if (!user) {
+      log.error(`user cancels operation`);
+      return;
+    }
+    useUser = user;
   }
   if (!useUser)
     return log.error(`${name} not found`);
